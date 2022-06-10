9f44874c3c8ca77f894847fb0f0c308aecc597d9
samuel14luis <luismendoza.samuel@gmail.com>
2022-06-09 21:49:12 -0500

    java base generator

---
+++ generate/spring.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd /opt/openapi-generator/modules/openapi-generator-cli/target
+echo $PWD
+java -jar openapi-generator-cli.jar generate \
+    -g spring \
+    --library spring-boot \
+    -i /local/petstore.yaml \
+    -o /local/out/java \
+    -p groupId=com.example \
+    -p artifactId=petstore \
+    -p artifactVersion=0.0.1-SNAPSHOT \
+    \
+    -p basePackage=com.example.petstore \
+    -p configPackage=com.example.petstore.config \
+    -p apiPackage=com.example.petstore.api \
+    -p modelPackage=com.example.petstore.model \
+    \
+    -p sourceFolder=src/main/gen \
+    \
+    -p dateLibrary=java11 \
+    -p java11=true
